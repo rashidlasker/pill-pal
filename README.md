0f0934a96ebea3c7f4907301fd9a185e9a2ef4a4
Rashid Lasker <rashidlasker@gmail.com>
2017-09-09 19:52:38 -0400

    slim-down

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Charlottesville Guide
+# Pill Pal
 
-Charlottesville Guide is an Amazon Alexa skill that provides information about Charlottesville and other things to do.
+Pill Pal is an Amazon Alexa skill that ensures medical adherence through a shared network of responsibility.
 
 # Installation
 
@@ -9,52 +9,3 @@ You can enable this skill from your Alexa App.
 # Usage
 
 This skill uses [AWS Lambda](https://aws.amazon.com/lambda/), the [Alexa Skills Kit (ASK)](https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit), and the [ASK SDK](https://developer.amazon.com/public/community/post/Tx213D2XQIYH864/Announcing-the-Alexa-Skills-Kit-for-Node-js).
-The user of this skill will be able to ask things like:
-
-   * “Alexa, ask Charlottesville Guide what there is to do.”
-   * “Alexa, ask Charlottesville Guide about Monticello.”
-   * “Alexa, ask Charlottesville Guide for the top five things to do.”
-
-Examples of other utterances are listed below.
-
-```
-    getOverview more information about charlottesville
-    getOverview what's it like in charlottesville
-    getOverview tell me some facts about charlottesville
-    getOverview facts about charlottesville
-    getOverview overview of charlottesville
-    getOverview charlottesville facts
-    getOverview charlottesville overview
-
-    getTopFiveIntent tell me the top five things
-    getTopFiveIntent top five things to do
-    getTopFiveIntent give me the top five
-    getTopFiveIntent the top five
-    getTopFiveIntent top five
-    getTopFiveIntent the top five things to do
-
-    getAttractionIntent tell me a good attraction
-    getAttractionIntent where should I go
-    getAttractionIntent an attraction
-    getAttractionIntent attractions
-
-    getMoreInfoIntent open number {attraction}
-    getMoreInfoIntent number {attraction}
-    getMoreInfoIntent {attraction}
-```
-# Todo:
-
-Add restaurant function with the following locations:
-* jumpcville
-* love sushi king
-* bodos
-* lemongrass
-* citizen burger
-* brazos
-* c&o late night
-* shebeen
-* bebedero
-* oakhurst
-* tavern and grocery
-* afghan kabob
-* now and zen
--- src/index.js
+++ src/index.js
@@ -44,24 +44,6 @@ var output = "";
 
 var alexa;
 
-var attractions = [
-    { name: "IX Art Park", content: "Imagine a walk-through, sculptural, mural-festooned Mecca that's free and open for the public to wander, night and day. Welcome to the IX Art Park: a public, non-commercial, interactive space for spontaneous dreaming – for residents, visitors, families and solo acts.", location: "The park is located off the downtown mall. \n Street address: 522 2nd Street SE in Charlottesville, Virginia", contact: "info@ixartpark.com" },
-    { name: "Monticello", content: "Monticello was the primary plantation of Thomas Jefferson, the third President of the United States, who began designing and building Monticello at age 26 after inheriting land from his father. Today, guided tours are provided daily throughout the year.", location: "931 Thomas Jefferson Pkwy, Charlottesville, VA 22902", contact: "434 984 9822" },
-    { name: "The Rotunda", content: "The Rotunda at the University of Virginia was designed by Thomas Jefferson as the architectural and academic heart of the University’s community of scholars. He named the University’s original buildings the “Academical Village.” As the phrase suggests, the Academical Village is based on the Jeffersonian principle that learning is a lifelong process, and that interaction between faculty and students is vital to the pursuit of knowledge.", location: "1826 University Ave, Charlottesville, VA 22904", contact: "434 924 7969" },
-    { name: "The Downtown Mall", content: "Maintained by the Charlottesville Parks and Recreation Department, the Historic Downtown Mall is considered one of the finest urban parks in the country.  This pedestrian mall is home to a vibrant collection of more than 120 shops and 30 restaurants located in the historic buildings on and around old Main Street Charlottesville.", location: "248 Water St E, Charlottesville, VA 22902", contact: "downtown charlottesville dot net" },
-    { name: "Humpback Rocks", content: "Humpback Rock is a steep, but popular trail in the northern section of the Blue Ridge Parkway with great views. It is a must see for hikers in the area", location: "Humpback Rock, Rockfish, VA 22920", contact: "540 943 4716" },
-];
-
-var topFive = [
-    { number: "1", caption: "Watch the sunset at the Thursday Evening Sunset Series at Carter Mountain", more: "One of our favorite traditions on Carter Mountain is the Thursday Evening Sunset Series! Each Thursday from mid-May through the end of September, the Carter Mountain orchard stays open til 9pm. Join them for dinner, live music, wine and hard cider, hayrides, and the best view of a sunset anywhere! It’s free admission! Bring lawn chairs and blankets, or come early for a picnic table.", location: "1435 Carters Mountain Trail Charlottesville, VA 22901", contact: "434 977 1833" },
-    { number: "2", caption: "Get shopping at the Charlottesville City Market", more: "A producer only market of farmers, food vendors, crafts, and artisans since 1973 – The Charlottesville City Market is held every Saturday from 7:00am till Noon from April through December. City Market offers fresh produce, herbs, plants, grass fed meats, crafts, and baked goods from local vendors every Saturday, April – December.  The market features over 100 vendors every Saturday morning.", location: "Downtown Charlottesville at Water Street and South Street between 1st and 2nd.", contact: "434 970 3371" },
-    { number: "3", caption: "Watch a show at Jefferson Theater", more: "Located on Charlottesville’s historic Downtown Mall, the Jefferson Theater was established in 1912 as a live performance theater that played host to silent movies, vaudeville acts and a historic list of live performers, ranging from Harry Houdini to The Three Stooges. In addition, state‐of‐the-art sound and lighting systems and treatments to the theater’s acoustics make The Jefferson an excellent environment to experience live music. The venue now plays host to a range of genres including rock, bluegrass, reggae, country, metal and hip-hop.", location: "110 E Main St, Charlottesville, VA 22902", contact: "434 245 4980" },
-    { number: "4", caption: "Experience local food at Feast!", more: "Feast! is a specialty food store that has been serving the Central Virginia community since its inception in 2002. This independently owned store offers an in-house cafe, catering services, and handmade gift hampers for several occasions. Most of what is sold in the shop comes from local farmers and artisans, and it is fresh, seasonal, and of superb quality. You can taste a lot of what they have to offer in their nice little café where they sell excellent sandwiches and soups. Ask for their daily specials.", location: "416 West Main St., Suite H Charlottesville, VA 22903 ", contact: "info@feastvirginia.com \n 434 244 7800 " },
-    { number: "5", caption: "Visit the Academical Village at the University of Virginia.", more: "The University of Virginia’s Academical Village, which is nearing its 200th anniversary, is unique in many respects, not the least of which is that it is the vision of one man, Thomas Jefferson, who conceived of the University and designed it.", location: "1826 University Ave, Charlottesville, VA 22904", contact: "virginia.edu" }
-];
-
-var topFiveIntro = "Here are the top five things to  do in " + location + ".";
-
 var newSessionHandlers = {
     'LaunchRequest': function () {
         this.handler.state = states.SEARCHMODE;
@@ -72,10 +54,6 @@ var newSessionHandlers = {
         this.handler.state = states.SEARCHMODE;
         this.emitWithState('getOverview');
     },
-//    'getNewsIntent': function () {
-//        this.handler.state = states.SEARCHMODE;
-//        this.emitWithState('getNewsIntent');
-//    },
     'getAttractionIntent': function () {
         this.handler.state = states.SEARCHMODE;
         this.emitWithState('getAttractionIntent');
@@ -147,44 +125,6 @@ var startSearchHandlers = Alexa.CreateStateHandler(states.SEARCHMODE, {
         output = HelpMessage;
         this.emit(':ask', output, HelpMessage);
     },
-//    'getNewsIntent': function () {
-//        httpGet(location, function (response) {
-//
-//            // Parse the response into a JSON object ready to be formatted.
-//            var responseData = JSON.parse(response);
-//            var cardContent = "Data provided by New York Times\n\n";
-//
-//            // Check if we have correct data, If not create an error speech out to try again.
-//            if (responseData == null) {
-//                output = "There was a problem with getting data please try again";
-//            }
-//            else {
-//                output = newsIntroMessage;
-//
-//                // If we have data.
-//                for (var i = 0; i < responseData.response.docs.length; i++) {
-//
-//                    if (i < numberOfResults) {
-//                        // Get the name and description JSON structure.
-//                        var headline = responseData.response.docs[i].headline.main;
-//                        var index = i + 1;
-//
-//                        output += " Headline " + index + ": " + headline + ";";
-//
-//                        cardContent += " Headline " + index + ".\n";
-//                        cardContent += headline + ".\n\n";
-//                    }
-//                }
-//
-//                output += " See your Alexa app for more information.";
-//            }
-//
-//            var cardTitle = location + " News";
-//
-//            alexa.emit(':tellWithCard', output, cardTitle, cardContent);
-//        });
-//    },
-
     'AMAZON.RepeatIntent': function () {
         this.emit(':ask', output, HelpMessage);
     },
@@ -242,15 +182,6 @@ var topFiveHandlers = Alexa.CreateStateHandler(states.TOPFIVE, {
         } else {
             this.emit(':ask', noAttractionErrorMessage);
         }
-    },
-
-    'AMAZON.YesIntent': function () {
-        output = getMoreInfoMessage;
-        alexa.emit(':ask', output, getMoreInfoRepromtMessage);
-    },
-    'AMAZON.NoIntent': function () {
-        output = goodbyeMessage;
-        alexa.emit(':tell', output);
     },
     'AMAZON.StopIntent': function () {
         this.emit(':tell', goodbyeMessage);
