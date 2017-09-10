c75f60c1bf0df9741105f864539f0e4dfaa133f7
Rashid Lasker <rashidlasker@gmail.com>
2017-09-09 20:25:27 -0400

    update intents

--- src/index.js
+++ src/index.js
@@ -1,42 +1,19 @@
 var Alexa = require('alexa-sdk');
 var http = require('http');
 
-var states = {
-    SEARCHMODE: '_SEARCHMODE',
-    TOPFIVE: '_TOPFIVE',
-};
+var welcomeMessage = "Hello! You can ask me for help. What will it be?";
 
-var location = "Charlottesville";
+var welcomeReprompt = "You can ask me for your schedule or say help. What will it be?";
 
-var numberOfResults = 3;
+var HelpMessage = "Here are some things you can say: Check if I've taken my medicine. Refill my prescriptions. Contact my doctor.  What would you like to do?";
 
-var APIKey = "4844d21f760b47359945751b9f875877";
-
-var welcomeMessage = location + " Guide. You can ask me for an attraction or say help. What will it be?";
-
-var welcomeRepromt = "You can ask me for an attraction or say help. What will it be?";
-
-var locationOverview = "Charlottesville is an independent city in the Commonwealth of Virginia. As of the 2010 census, the population was 48,210. It is the county seat of Albemarle County, which surrounds the city, though the two are separate legal entities. It is named after the British Queen Charlotte of Mecklenburg-Strelitz. Charlottesville was the home of two Presidents, Thomas Jefferson and James Monroe. While both served as Governor of Virginia, they lived in Charlottesville, and traveled to and from Richmond, along the 71-mile historic Three Notch'd Road. Charlottesville is well known for its restaurants, vineyards, as well as the University of Virginia.";
-
-var HelpMessage = "Here are some things you can say: Give me an attraction. Tell me about " + location + ". Tell me the top five things to do.  What would you like to do?";
-
-var moreInformation = "See your  Alexa app for  more  information."
+var moreInformation = "See your Alexa app for  more  information."
 
 var tryAgainMessage = "please try again."
 
-var noAttractionErrorMessage = "What attraction was that? " + tryAgainMessage;
+var noMedicineErrorMessage = "What medicine was that? " + tryAgainMessage;
 
-var topFiveMoreInfo = " You can tell me a number for more information. For example, open number one.";
-
-var getMoreInfoRepromtMessage = "What number attraction would you like to hear about?";
-
-var getMoreInfoMessage = "OK, " + getMoreInfoRepromtMessage;
-
-var goodbyeMessage = "OK, have a nice time in " + location + ".";
-
-//var newsIntroMessage = "These are the " + numberOfResults + " most recent " + location + " headlines, you can read more on your Alexa app. ";
-
-var hearMoreMessage = "Would you like to hear about another top thing that you can do in " + location +"?";
+var goodbyeMessage = "OK, good bye.";
 
 var newline = "\n";
 
@@ -46,21 +23,40 @@ var alexa;
 
 var newSessionHandlers = {
     'LaunchRequest': function () {
-        this.handler.state = states.SEARCHMODE;
-        output = welcomeMessage;
-        this.emit(':ask', output, welcomeRepromt);
+        output = "hello 1";
+        this.emit(':ask', output, welcomeReprompt);
     },
-    'getOverview': function () {
-        this.handler.state = states.SEARCHMODE;
-        this.emitWithState('getOverview');
+    'CheckIntent': function () {
+        output = "hello 2";
+        this.emit(':tell', output);
     },
-    'getAttractionIntent': function () {
-        this.handler.state = states.SEARCHMODE;
-        this.emitWithState('getAttractionIntent');
+    'ConfirmAdherenceIntent': function () {
+        output = "hello 3";
+        this.emit(':tell', output);
     },
-    'getTopFiveIntent': function(){
-        this.handler.state = states.SEARCHMODE;
-        this.emitWithState('getTopFiveIntent');
+    'DoctorContactIntent': function(){
+        output = "hello 4";
+        this.emit(':tell', output);
+    },
+    'DoctorIntent': function(){
+        output = "hello 5";
+        this.emit(':tell', output);
+    },
+    'ManualAdherenceIntent': function(){
+        output = "hello 6";
+        this.emit(':tell', output);
+    },
+    'NextDoseIntent': function(){
+        output = "hello 7";
+        this.emit(':tell', output);
+    },
+    'PharmacyInfoIntent': function(){
+        output = "hello 8";
+        this.emit(':tell', output);
+    },
+    'RefillIntent': function(){
+        output = "hello 9";
+        this.emit(':tell', output);
     },
     'AMAZON.StopIntent': function () {
         this.emit(':tell', goodbyeMessage);
@@ -78,167 +74,12 @@ var newSessionHandlers = {
         this.emit(':ask', output, welcomeRepromt);
     },
 };
-
-var startSearchHandlers = Alexa.CreateStateHandler(states.SEARCHMODE, {
-    'getOverview': function () {
-        output = locationOverview;
-        this.emit(':tellWithCard', output, output, locationOverview);
-    },
-    'getAttractionIntent': function () {
-        var cardTitle = location;
-        var cardContent = "";
-
-        var attraction = attractions[Math.floor(Math.random() * attractions.length)];
-        if (attraction) {
-            output = attraction.name + " " + attraction.content + newline + moreInformation;
-            cardTitle = attraction.name;
-            cardContent = attraction.content + newline + attraction.contact;
-
-            this.emit(':tellWithCard', output, cardTitle, cardContent);
-        } else {
-            this.emit(':ask', noAttractionErrorMessage, tryAgainMessage);
-        }
-    },
-    'getTopFiveIntent': function () {
-        output = topFiveIntro;
-        var cardTitle = "Top Five Things To See in " + location;
-
-        for (var counter = topFive.length - 1; counter >= 0; counter--) {
-            output += " Number " + topFive[counter].number + ": " + topFive[counter].caption + newline;
-        }
-        output += topFiveMoreInfo;
-        this.handler.state = states.TOPFIVE;
-        this.emit(':askWithCard', output, topFiveMoreInfo, cardTitle, output);
-    },
-    'AMAZON.YesIntent': function () {
-        output = HelpMessage;
-        this.emit(':ask', output, HelpMessage);
-    },
-    'AMAZON.NoIntent': function () {
-        output = HelpMessage;
-        this.emit(':ask', HelpMessage, HelpMessage);
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', goodbyeMessage);
-    },
-    'AMAZON.HelpIntent': function () {
-        output = HelpMessage;
-        this.emit(':ask', output, HelpMessage);
-    },
-    'AMAZON.RepeatIntent': function () {
-        this.emit(':ask', output, HelpMessage);
-    },
-    'AMAZON.CancelIntent': function () {
-        // Use this function to clear up and save any data needed between sessions
-        this.emit(":tell", goodbyeMessage);
-    },
-    'SessionEndedRequest': function () {
-        // Use this function to clear up and save any data needed between sessions
-        this.emit('AMAZON.StopIntent');
-    },
-    'Unhandled': function () {
-        output = HelpMessage;
-        this.emit(':ask', output, welcomeRepromt);
-    }
-});
-
-var topFiveHandlers = Alexa.CreateStateHandler(states.TOPFIVE, {
-    'getAttractionIntent': function () {
-        this.handler.state = states.SEARCHMODE;
-        this.emitWithState('getAttractionIntent');
-    },
-    'getOverview': function () {
-        this.handler.state = states.SEARCHMODE;
-        this.emitWithState('getOverview');
-    },
-    'getTopFiveIntent': function () {
-        this.handler.state = states.SEARCHMODE;
-        this.emitWithState('getTopFiveIntent');
-    },
-    'AMAZON.HelpIntent': function () {
-        output = HelpMessage;
-        this.emit(':ask', output, HelpMessage);
-    },
-
-    'getMoreInfoIntent': function () {
-        var slotValue = 0;
-        if(this.event.request.intent.slots.attraction ) {
-            if (this.event.request.intent.slots.attraction.value) {
-                slotValue = this.event.request.intent.slots.attraction.value;
-
-            }
-        }
-
-        if (slotValue > 0 && slotValue <= topFive.length) {
-
-            var index = parseInt(slotValue) - 1;
-            var selectedAttraction = topFive[index];
-
-            output = selectedAttraction.caption + ". " + selectedAttraction.more + ". " + hearMoreMessage;
-            var cardTitle = selectedAttraction.name;
-            var cardContent = selectedAttraction.caption + newline + newline + selectedAttraction.more + newline + newline + selectedAttraction.location + newline + newline + selectedAttraction.contact;
-
-            this.emit(':askWithCard', output, hearMoreMessage, cardTitle, cardContent);
-        } else {
-            this.emit(':ask', noAttractionErrorMessage);
-        }
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', goodbyeMessage);
-    },
-    'AMAZON.RepeatIntent': function () {
-        this.emit(':ask', output, HelpMessage);
-    },
-    'AMAZON.CancelIntent': function () {
-        // Use this function to clear up and save any data needed between sessions
-        this.emit(":tell", goodbyeMessage);
-    },
-    'SessionEndedRequest': function () {
-        // Use this function to clear up and save any data needed between sessions
-    },
-
-    'Unhandled': function () {
-        output = HelpMessage;
-        this.emit(':ask', output, welcomeRepromt);
-    }
-});
 
 exports.handler = function (event, context, callback) {
     alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(newSessionHandlers, startSearchHandlers, topFiveHandlers);
+    alexa.registerHandlers(newSessionHandlers);
     alexa.execute();
 };
-
-// Create a web request and handle the response.
-function httpGet(query, callback) {
-    console.log("/n QUERY: "+query);
-
-    var options = {
-        //http://api.nytimes.com/svc/search/v2/articlesearch.json?q=seattle&sort=newest&api-key=
-        host: 'api.nytimes.com',
-        path: '/svc/search/v2/articlesearch.json?q=' + query + '&sort=newest&api-key=' + APIKey,
-        method: 'GET'
-    };
-
-    var req = http.request(options, (res) => {
-
-            var body = '';
-
-    res.on('data', (d) => {
-        body += d;
-});
-
-    res.on('end', function () {
-        callback(body);
-    });
-
-});
-    req.end();
-
-    req.on('error', (e) => {
-        console.error(e);
-});
-}
 
 String.prototype.trunc =
     function (n) {
