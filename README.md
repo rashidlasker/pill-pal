588b36829968b18277a1e084360331482dfbf0f4
Rashid Lasker <rashidlasker@gmail.com>
2017-09-10 06:11:54 -0400

    Merge

--- README.md
+++ README.md
@@ -1,11 +1,39 @@
-# Pill Pal
+# PillPal - Medication Adherence, Managed
 
-Pill Pal is an Amazon Alexa skill that ensures medical adherence through a shared network of responsibility.
+## The Problem
+The elderly represent one of the most medically vulnerable demographics as a result of their increased chances of multiple comorbidities. Elderly patients therefore have a propensity for polypharmacy, with 20% of people over the age of 65 taking upwards of ten medications each day. Medication adherence among the elderly is thus an incredibly important issue. However, it continues to be a problem for a whole host of reasons. Perhaps the most worrisome of these reasons is deteriorating memory. In far too many cases, elders forget to take necessary medications or, tragically, forget that they already took the medication and overdose. 
 
-# Installation
+## The Solution
+PillPal is a comprehensive suite of tools that develop a shared network of responsibility to remind patients of the times they should be taking their medications as well as their prescribed dosages. On the patient-side, the system uses Alexa to give a vocal reminder of which medications should be taken at which time. Because mobile interfaces can be difficult to use for this demographic, we wanted our project to provide a sense of interpersonal communication, improving accessibility. A series of voice-enabled commands allow patients to learn more about the drugs they're taking, why they're taking them, and how often they should be taken.
 
-You can enable this skill from your Alexa App.
+Furthermore, the product integrates caretaker interactions by sending push notifications to help them monitor medication adherence and work concurrently with clinicians. From a physician's point of view, PillPal's records can be accessed and updated so they remain reliable and up-to-date. This mitigates miscommunication and encourages the sharing of vital data.
 
-# Usage
+## How we built it
+Our team used a number of AWS services - namely, Alexa connected with Lambda functions, as well as a DynamoDB database, CloudWatch events, and SNS push notifications. We also made an interactive proof-of-concept for the web/mobile app design using Figma and Invision.
 
-This skill uses [AWS Lambda](https://aws.amazon.com/lambda/), the [Alexa Skills Kit (ASK)](https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit), and the [ASK SDK](https://developer.amazon.com/public/community/post/Tx213D2XQIYH864/Announcing-the-Alexa-Skills-Kit-for-Node-js).
+Doctors and caregivers are able to access and modify database records for their patient through the web app. Alexa interfaces with this, updating once a minute, to tell patients in real time when they should next be taking their medication based on DynamoDB records.
+
+## What's next for PillPal
+- Pill dispensers that dispense the exact amount of medication at given times exist, but are very expensive, fetching upwards of $1000. In the future, we hope to interface PillPal with a physical dispenser to drive down costs and improve accessibility for such a product.
+- We also hope to develop a native mobile app interface for physicians/medical personnel and caregivers to communicate with PillPal seamlessly.
+- To ensure HIPPA compiance, we wish to secure the system of communication with state-of-the-art cybersecurity methodologies.
+
+## Challenges we ran into
+One major decision we had to make early on was choosing between a Node.js and a Python-based development environment. Our team members were more experienced with Python, but ultimately we decided to work within a Node.js environment to maximize web compatibility, deal with dependencies, and follow the most up-to-date conventions for web development.
+
+Another major decision we had to make was determining the database format for our data. We had to choose between a relational SQL database (with Amazon Aurora) and a non-relational NoSQL database (with DynamoDB). In the end, we went with a NoSQL database because of its ability to be exported as a JSON file, allowing for the future development of PillPal as a REST-ful API.
+
+Later on, we ran into problems with asynchronous processes on Alexa. These processes initially ran too slow and caused missing statements within our code. Ultimately, we solved this problem by forcing the asynchronous processes to load before continuing with the skill logic. We found that the delay caused by this was imperceptible, and continued with our development.
+
+## Accomplishments that we're proud of
+One thing we're proud of is the fact that we created a full system for the issue of medication adherence. Coming in, each of us only expected to make a standalone app at most. This shift in focus during the hackathon is important because major changes aren't caused by apps, they're caused by new systems of approach. We hope to expand PillPal into more and more areas of interest, strengthening its ability to help the people who need it most.
+
+## What we learned
+More than anything, we learned how to wire together a whole suite of AWS products. This, in turn, allowed us to diversify our initial product. We specifically thank Tyler Lynch and Paul Curcio for guiding us on this path.
+
+## Other benefits of PillPal
+Outside of medication adherence, this app can improve communication between elderly patients and their physicians. It is not uncommon for elderly patients to leave meetings with their doctors without complete awareness of what they have been told to do. This may include exercise regiments or diet restrictons.
+
+##Sources
+http://www.sciencedirect.com/science/article/pii/S2210833515000441
+http://www.pharmacytimes.com/publications/issue/2011/january2011/rxfocus-0111
--- src/index.js
+++ src/index.js
@@ -7,21 +7,20 @@ var tableName = "user";
 
 var client_name = "";
 
-var welcomeMessage = "Hello! You can ask me for help. What will it be?";
-
 var welcomeReprompt = "You can ask me for your schedule or say help. What will it be?";
 
 var HelpMessage = "Here are some things you can say: Check if I've taken my medicine. Refill my prescriptions. Contact my doctor.  What would you like to do?";
 
-var moreInformation = "See your Alexa app for  more  information."
+var moreInformation = "See your Alexa app for more information."
 
-var pharmacyInformation;
+var tryAgainMessage = "Sorry, I didn't get that. Please try again."
 
-var doctorInformation;
-
-var tryAgainMessage = "please try again."
+var medRemindReprompt = "Sorry, I didn't get that. Would you like to know what medications you still have to take? "
 
 var noMedicineErrorMessage = "Sorry, we couldn't find that medicince in the file. " + tryAgainMessage;
+
+var promptQuestion = ["Do you have another question you'd like me to answer?", "Is there another question you'd like to ask?", "Is there anything else you want to ask?",
+"Do you have another question?", "What else can I do for you?"];
 
 var goodbyeMessage = "OK, good bye.";
 
@@ -35,6 +34,21 @@ var pills_left;
 
 var client_intervals;
 
+var medNames;
+
+var nodeNum;
+
+var medications;
+
+var pharmacyInformation;
+
+var doctorInformation;
+
+var states = {
+    ASKMODE: '_ASKMODE',
+    STARTMODE: '_STARTMODE'
+};
+
 exports.handler = function (event, context, callback) {
     alexa = Alexa.handler(event, context);
     dynamodb.scan({
@@ -44,21 +58,33 @@ exports.handler = function (event, context, callback) {
         if (err) {
             context.done('error','reading dynamodb failed: '+err);
         }
+        // change data.Items[0] to specific person
+        var allMeds = data.Items[0]["meds"].L;
+        medNames = [];
+        for (var i in allMeds) {
+            var medInfo = allMeds[i].M;
+            medNames.push(medInfo.name.S);
+        }
         client_name = data.Items[0]["client_name"].S.split(" ");
         pills_left = data.Items[0]["times_left"].L;
         client_intervals = data.Items[0]["intervals"];
         pharmacyInformation = data.Items[0]["pharmacy"];
         doctorInformation = data.Items[0]["doctor"];
-        alexa.registerHandlers(newSessionHandlers);
+        alexa.registerHandlers(newSessionHandlers, startSessionHandlers, askSessionHandlers);
         alexa.execute();
     });
 };
-
 var newSessionHandlers = {
-    'LaunchRequest': function () {
-        output = "Hello, " + client_name[0];
-        this.emit(':ask', output, welcomeReprompt);
+    'LaunchRequest': function() {
+        this.handler.state = states.STARTMODE;
+        output = "Hello, " + client_name[0] + ", " + welcomeReprompt;
+        this.emit(':ask', output , welcomeReprompt);
     },
+    'Unhandled': function () {
+        this.emit(':ask', HelpMessage, HelpMessage);
+    }
+}
+var startSessionHandlers = Alexa.CreateStateHandler(states.STARTMODE, {
     'CheckIntent': function () {
         isDone = true;
         for (var x in pills_left){
@@ -67,58 +93,96 @@ var newSessionHandlers = {
             } 
         }
         if (isDone) {
-            appendMSG = "Congratulations! You've finished your medications for today."
+            output = "Congratulations! You've finished your medications for today. "
+            var promptIndex = Math.floor(Math.random() * promptQuestion.length);
+            this.emit(':ask', output + promptQuestion[promptIndex], promptQuestion[promptIndex]);
         } else {
-            appendMSG = "Unfortunately, you still have unfinished medications."
+            appendMSG = "Unfortunately, you still have unfinished medications. Would you like to know what medications you still have to take? "
+            this.handler.state = states.ASKMODE;
+            nodeNum = 0;
+            this.emit(':ask', appendMSG, medRemindReprompt);           
         }
-        output = "Hey, " + client_name[0] + appendMSG;
-        this.emit(':tell', output);
     },
     'ConfirmAdherenceIntent': function () {
-        output = "how do i confirm lmao";
+        output = "Confirm. ";
         this.emit(':tell', output);
     },
     'DoctorContactIntent': function(){
-        output = "I am sending an SMS to your doctor.";
-        this.emit(':tell', output);
+        output = "I am sending an SMS to your doctor. ";
+        var promptIndex = Math.floor(Math.random() * promptQuestion.length);
+        this.emit(':ask', output + promptQuestion[promptIndex], promptQuestion[promptIndex]);
     },
     'DoctorIntent': function(){
         output = doctorInformation;
-        this.emit(':tell', output);
+        var promptIndex = Math.floor(Math.random() * promptQuestion.length);
+        this.emit(':ask', output + promptQuestion[promptIndex], promptQuestion[promptIndex]);
     },
     'ManualAdherenceIntent': function(){
         output = "ignore this";
         this.emit(':tell', output);
     },
     'NextDoseIntent': function(){
-        output = "your next dose is right now bihh";
+        output = "Your next dose is scheduled at " +  "";
         this.emit(':tell', output);
     },
     'PharmacyInfoIntent': function(){
         output = pharmacyInformation;
-        this.emit(':tell', output);
+        var promptIndex = Math.floor(Math.random() * promptQuestion.length);
+        this.emit(':ask', output + promptQuestion[promptIndex], promptQuestion[promptIndex]);
     },
     'RefillIntent': function(){
-        //send prescription request
         output = "I refilled your prescription. You should get a confirmation soon.";
-        this.emit(':tell', output);
-    },
+        var promptIndex = Math.floor(Math.random() * promptQuestion.length);
+        this.emit(':ask', output + promptQuestion[promptIndex], promptQuestion[promptIndex]);    },
     'AMAZON.StopIntent': function () {
         this.emit(':tell', goodbyeMessage);
     },
     'AMAZON.CancelIntent': function () {
         // Use this function to clear up and save any data needed between sessions
-        this.emit(":tell", goodbyeMessage);
+        this.emit(':tell', goodbyeMessage);
     },
     'SessionEndedRequest': function () {
         // Use this function to clear up and save any data needed between sessions
         this.emit('AMAZON.StopIntent');
     },
-    'Unhandled': function () {
-        output = HelpMessage;
-        this.emit(':ask', output, welcomeRepromt);
+    'AMAZON.YesIntent': function () {
+        response = "Awesome. What question would you like me to answer?";
+        this.emit(':ask',response, HelpMessage);
     },
-};
+    'AMAZON.NoIntent': function () {
+        this.emit(':tell',goodbyeMessage);
+    },
+    'Unhandled': function () {
+        this.emit(':ask', HelpMessage, welcomeReprompt);
+    },
+});
+var askSessionHandlers = Alexa.CreateStateHandler(states.ASKMODE, {
+    'AMAZON.YesIntent': function () {
+        var response;
+        var promptIndex;
+        switch(nodeNum) {
+            case 0:
+                medications = ""
+                for (var i in medNames) {
+                    if (i < medNames.length - 1) {
+                        medications = medications + " " + pills_left[i].N.toString() + " doses of " + medNames[i] +",";
+                    } else {
+                        medications = medications + "and " + pills_left[i].N.toString() + " doses of " + medNames[i];
+                    }
+                }
+                promptIndex = Math.floor(Math.random() * promptQuestion.length);
+                response = "You have " + medications + " left for today. " + promptQuestion[promptIndex];
+                break;
+        }
+        this.handler.state = states.STARTMODE;
+        nodeNum = 0;
+        this.emit(':ask',response, promptQuestion[promptIndex])
+    },
+    'AMAZON.NoIntent': function () {
+        this.emit(':tell', "Okay. Make sure to not forget!")
+    }
+});
+
 String.prototype.trunc =
     function (n) {
         return this.substr(0, n - 1) + (this.length > n ? '&hellip;' : '');
